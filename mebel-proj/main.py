a781734d69f7a036efa48f60413b61660adf0c8b
123 <cartema1329@gmail.com>
2022-04-24 00:46:15 +0300

    почки отказали, сил больше нет, час ночи

--- mebel-proj/main.py
+++ mebel-proj/main.py
@@ -12,7 +12,7 @@ def news():
     with open("package.json", "rt", encoding="utf8") as f:
         news_list = json.loads(f.read())
     print(news_list)
-    return render_template('notes.html', news=news_list)
+    return render_template('mades.html', news=news_list)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
