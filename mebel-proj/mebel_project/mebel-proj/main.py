e3382aae4c348d00b38c3cd8d1e20bb04505ef65
ArtemMoshenko <91259325+ArtemMoshenko@users.noreply.github.com>
2022-04-24 00:44:05 +0300

    Add files via upload

---
+++ mebel-proj/mebel_project/mebel-proj/main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+import json
+from flask import url_for
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def news():
+    with open("package.json", "rt", encoding="utf8") as f:
+        news_list = json.loads(f.read())
+    print(news_list)
+    return render_template('mades.html', news=news_list)
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
